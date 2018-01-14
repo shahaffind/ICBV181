a670e32225da2e6c917cfa51db04116e395191e4
Shahaf Finder <shahaffind@gmail.com>
2018-01-14 17:08:05 +0200

    some improvements in code order

--- ass3/Segment.m
+++ ass3/Segment.m
@@ -1,35 +1,22 @@
 function [segmentation_array] = Segment(I)
     % magic #s
     n_labels = 24;
+    max_iter = 10000;
+    min_change = 10^-5;
     
     p_k = init_p_0(I, n_labels);
     s_k = calculate_support(p_k, size(I));
-    m_1 = ones(size(p_k));
     
-    % normalize probabilities
-    numerator = p_k .* s_k;
-    s_numerator = sum(numerator);
-    denominator = m_1;
-    for i = 1:length(I(:))
-        denominator(:,i) = s_numerator(i) * m_1(:,i);
-    end
+    p_k_next = calculate_p_k_next(p_k, s_k);
     
-    p_k_next = numerator ./ denominator;
     delta = abs(p_k - p_k_next); % changes made
     
     iter = 1;
-    while (max(delta(:)) > (0.00001) && iter < 1000)
+    while max(delta(:)) > min_change && iter < max_iter
         p_k = p_k_next;
         s_k = calculate_support(p_k, size(I));
         
-        % normalize probabilities
-        numerator = (p_k .* s_k);
-        s_numerator = sum(numerator);
-        denominator = m_1;
-        for i = 1:length(I(:))
-            denominator(:,i) = s_numerator(i) * m_1(:,i);
-        end
-        p_k_next = numerator ./ denominator;
+        p_k_next = calculate_p_k_next(p_k, s_k);
         delta = abs(p_k - p_k_next); % changes made
         iter = iter + 1;
     end
@@ -42,15 +29,16 @@ function [segmentation_array] = Segment(I)
         labels(i) = sorted_idx(1);
     end
     
-    test = reshape(labels, size(I)) / 24;
+    test = reshape(labels, size(I)) / n_labels;
     imshow(test);
+    disp(iter);
 end
 
 function [p_0] = init_p_0(I, n_labels)
     [~, Gdir] = imgradient(I);
     
     % n_labels possible gradients
-    I_first_label = floor(mod(Gdir ./ (360 / n_labels), n_labels)) + 1;
+    I_first_label = round(mod(Gdir ./ (360 / n_labels), n_labels)) + 1;
     p_0 = zeros(length(I_first_label(:)), n_labels);
     
     % create prob window and set prob for each pixel
@@ -79,18 +67,32 @@ function [w] = CreateWindow(size)
 end
 
 function [s] = calculate_support(p_k, im_dim)
+    % init
     [n_labels, n_pix] = size(p_k);
-    s = zeros(n_labels, n_pix);
+    raw_support = zeros(n_labels, n_pix);
     
+    % calculate support for each pixel for label j using conv
     for j = 1: n_labels
         p_k_label = p_k(j,:);
         p_k_label = reshape(p_k_label, im_dim);
-%         ker = fspecial('gaussian', 5, 1);
-%         ker = ker / ker(3,3);
         ker = ones(3);
-        support_label = conv2(p_k_label, ker);
-        support_label = support_label(2:im_dim(1)+1, 2:im_dim(2)+1); %trim
-        s(j,:) = support_label(:);
+        support_row = conv2(p_k_label, ker, 'same');
+        raw_support(j,:) = support_row(:);
+    end
+    s = raw_support;
+    
+end
+
+function [p_k_next] = calculate_p_k_next(p_k, s_k)
+    size_p_k = size(p_k);
+    
+    numerator = (p_k .* s_k);
+    s_numerator = sum(numerator);
+    denominator = ones(size_p_k);
+    
+    for i = 1:size_p_k(2)
+        denominator(:,i) = s_numerator(i) * denominator(:,i);
     end
     
+    p_k_next = numerator ./ denominator;
 end
